8faddbe5803aee90bcb92383c93cc7948ac31458
woogamjaa <woogamjaa@gmail.com>
2025-06-09 23:52:05 +0900

    first commit

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,10 @@
+// src/stories/App.stories.jsx
+import React from 'react';
+import App from '../App';
+
+export default {
+  title: 'Layout/App',
+  component: App,
+};
+
+export const Default = () => <App />;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: 'class', // 'media' 도 가능하지만 보통 'class'를 많이 씁니다.
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
